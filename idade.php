bd33f9bd06bc1cc9cb8b1820937e857e0c1dc60d
André Luís Tavares Mossignato <andreluis.mossignato@gmail.com>
2025-06-03 20:58:52 -0300

    alterações em aula dia 03.06

--- idade.php
+++ idade.php
@@ -4,25 +4,31 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Idade</title>
+    <link rel="stylesheet" href="estilos/idade.css">
 </head>
 <body>
-    <h2>Calculadora de Idade</h2>
 
-    <form action="idade.php" method="post">
-        <label for="nascimento">Data de Nascimento:</label><br>
-        <input type="date" name="nascimento" id="nascimento" required><br><br>
-        <input type="submit" value="Calcular Idade">
-    </form>
+  <h2>Calculadora de Idade</h2>
+
+  <form action="idade.php" method="post">
+    <label for="nascimento">Data de Nascimento:</label><br>
+    <input type="date" name="nascimento" id="nascimento" required><br><br>
+    <input type="submit" value="Calcular Idade">
+  </form>
+  <a href="index.html" class="btn-voltar">Voltar para a Página Principal</a>
 
     <?php
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $dataNascimento = $_POST["nascimento"];
-        
+
         if ($dataNascimento) {
             $dataNascimentoObj = new DateTime($dataNascimento);
             $hoje = new DateTime();
             $idade = $hoje->diff($dataNascimentoObj)->y;
             echo "<p>Você tem <strong>$idade anos</strong>.</p>";
+            if ($dataNascimentoObj->format('d-m') === $hoje->format('d-m')) {
+                echo "<p><strong>Hoje é seu aniversário, parabéns!! 🥳🎉</strong></p>";
+            }
         } else {
             echo "<p>Porfavor , insira uma data valida.</p>";
         }
--- temperatura.php
+++ temperatura.php
@@ -4,17 +4,49 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>temperatura</title>
+    <link rel="stylesheet" href="estilos/temperatura.css">
 </head>
 <body>
 <form action="temperatura.php" method="post">
-    <label for="celcius">graus </label><br>
-    <input type="number" name="celcius" id="celcius" required class="formu"> 
+    <div id="calculo">
+        <label for="celcius"></label><br>
+        <select name="tipo">
+        <option value="1">Celsius para Fahrenheit</option>
+        <option value="2">Fahrenheit para Celsius</option>
+        <option value="3">Celsius para Kelvin</option>
+        </select>
+    </div>
+    <div id="calculo2">
+    <br>
+    <label for="resultado" id="resultado"></label>
+    <input type="number" id="caixa" name="resultado" placeholder="" required> <br>
+    <button type="submit">Calcular</button>
+    </div> 
 
     <br>
 </form>
 
-<?php
 
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $resultado = $_POST['resultado'];
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $TSelecionada = $_POST['tipo'];
+
+        if ($TSelecionada == "1") {
+            $FAH = ($resultado * 9/5) + 32;
+            echo "Resultado em Fahrenheit: $FAH";
+        } elseif ($TSelecionada == "2") {
+            $CEL = ($resultado - 32) * 5/9;
+            echo "Resultado em Celsius: $CEL";
+        } elseif ($TSelecionada == "3") {
+            $KEL = ($resultado + 273.15);
+            echo "Resultado em Kelvin: $KEL";
+        } else {
+            echo "erro";  
+        }
+    }
+}
 ?>    
  
 </body>
